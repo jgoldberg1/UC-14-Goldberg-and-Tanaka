df02b297d3622f7867185f55c47d3b66ec495398
Jenna M Goldberg 9 <jg32137@srcs.loc>
2017-04-19 14:17:49 -0700

    4/19/17

--- calc_masterObjects.js
+++ calc_masterObjects.js
@@ -9,113 +9,63 @@
  * 8. Super Challenge: Add ( and ) buttons that work!
  * 9. Super Duper Challenge: Add exponents (negatives too!)
  */
-function Calculator(inputId) {
-    this.memory = "0",
-    this.operator = 0,
-    this.currentInput = "0",
-    this.displayCurrentInput = function () {
-        document.getElementById(inputId).value = this.currentInput;
-    }
-    this.addDigit = function (dig) {
-        if ((eval(this.currentInput) == 0) && (this.currentInput.indexOf(".") == -1)) {
-            this.currentInput = dig;
-        }
-        else if (this.currentInput.length <= 11) {
-            this.currentInput = this.currentInput + dig;
-        }
-        this.displayCurrentInput;
-    }
-    this.addDecimal = function () {
-        if (this.currentInput.length == 0) {
-            //no leading ".", use "0."
-            this.currentInput = "0.";
-        }
-        else {
-            // First make sure one doesn't exist
-            if (this.currentInput.indexOf(".") == -1) {
-                this.currentInput = this.currentInput + ".";
-            }
-        }
-        this.displayCurrentInput;
+
+var calculator = {
+    inputs: {
+        oldInput: "0",
+        currentInput: document.getElementById('inputId'),
+
     },
-    this.allClear = function () {
-        this.currentInput = "0";
-        this.operator = 0; //clear operator
-        this.memory = "0"; //clear memory
-        this.displayCurrentInput;
+
+    operator:{
+        multiply: 1,
+        divide: 2,
+        add: 3,
+        subtract: 4
     },
-    this.storeOperator = function (op) {
-        if (op.indexOf("*") > -1) {
-            this.operator = 1;
-        }; //codes for *
-        if (op.indexOf("/") > -1) {
-            this.operator = 2;
-        }; // slash (divide)
-        if (op.indexOf("+") > -1) {
-            this.operator = 3;
-        }; // sum
-        if (op.indexOf("-") > -1) {
-            this.operator = 4;
-        }; // difference
-        this.memory = this.currentInput; //store value
-        this.currentInput = "0";
-        this.displayCurrentInput;
+
+    displayCurrentInput: function(inputId) {
+        document.getElementById('inputId') = inputs.currentInput;
+        console.log(inputs.currentInput);
     },
-    this.calculate = function () {
-        if (this.operator == 1) {
-            this.currentInput = eval(this.memory) * eval(this.currentInput);
-        }
-        if (this.operator == 2) {
-            if (this.currentInput == 0) {
-                this.currentInput = "DIVIDE BY ZERO ERROR"
-                this.currentInput.className = "alert alert-danger";
-            }
-            else {
-                this.currentInput = this.currentInput = eval(this.memory) / eval(this.currentInput);
-            }
-        }
-        if (this.operator == 3) {
-            this.currentInput = eval(this.memory) + eval(this.currentInput);
-        }
-        if (this.operator == 4) {
-            this.currentInput = eval(this.memory) - eval(this.currentInput);
-        };
-        this.operator = 0; //clear operator
-        this.memory = "0"; //clear memory
-        this.displayCurrentInput;
-    },
-    this.changeSign = function () {
-        this.currentInput = this.currentInput * (-1);
-        this.displayCurrentInput;
-    }
-    this.percentage = function () {
-        this.currentInput = this.currentInput / 100;
-        this.displayCurrentInput;
-    }
-    this.clears = function () {
-        this.currentInput = "0";
-        this.displayCurrentInput;
-    }
-    this.factorial = function () {
-        var input = this.currentInput;
-        var x = input;
-        for (i = input - 1; i > 1; i--) {
-            x = x * i;
-        }
-        this.currentInput = x;
-        console.log(this.currentInput);
-        this.displayCurrentInput;
-    }
-    this.square = function () {
-        this.currentInput = Math.pow(this.currentInput, 2);
-        this.displayCurrentInput;
-    }
-    this.squareRoot = function () {
-        this.currentInput = Math.sqrt(this.currentInput);
-        this.displayCurrentInput;
-    }
-    this.inverse = function () {
-        this.currentInput = 1 / this.currentInput;
-        this.displayCurrentInput;
-    }
+
+    allClear: function allClear() {
+        calculator.inputs.currentInput = "0";
+        calculator.operator.multiply = 0; //clear operator
+        calculator.inputs.oldInput = "0"; //clear memory
+        calculator.displayCurrentInput();
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+function Calculator(inputId) {
+    this.memory = "0";
+    this.operator = 0;
+    this.currentInput = "0";
+    this.displayCurrentInput = displayCurrentInput;
+    this.allClear = allClear
+}
+
+function displayCurrentInput(inputId) {
+    document.getElementById(inputId).value = this.currentInput;
+}
+
+function addDigit(dig) {
+    if ((eval(this.currentInput) == 0) && (this.currentInput.indexOf(".") == -1)) {
+        this.currentInput = dig;
+    }
+    else if (this.currentInput.length <= 11) {
+        this.currentInput = this.currentInput + dig;
+    }
+    this.displayCurrentInput();
+}
+
--- old calc js.js
+++ old calc js.js
@@ -284,3 +284,116 @@ function tangentRadian() {
     currentInput = Math.tan(currentInput);
     displayCurrentInput();
 }
+
+**/
+
+// Test adding one, then two decimals
+QUnit.test( "Add decimal test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDecimal();
+    Calculator.addDigit('2');
+    Calculator.addDecimal();
+    assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+});
+//US1: As a user, I want to be able to press a button and change the sign of the current number that I have inputted into the calculator.
+QUnit.test("Add sign change test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('2');
+    Calculator.changeSign();
+
+    assert.equal(document.getElementById("screen").value, "-2", "Passed - Expected -2");
+});
+
+
+//US2: As a user I want to be able to press a button and change the inputed number on the calculator's screen to change to a percentage.
+QUnit.test("Add percentage test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('2');
+    Calculator.percentage();
+
+    assert.equal(document.getElementById("screen").value, "0.02", "Passed - Expected 0.02");
+});
+
+//US3: As a user, I want to be able to calculate the inverse of a number simply by pressing a button.
+QUnit.test("Add inverse test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('2');
+    Calculator.inverse();
+
+    assert.equal(document.getElementById("screen").value, "0.5", "Passed - Expected 0.5");
+});
+
+
+//US4: As a user I want to be able to calculate the factorial of a number by simply pressing a button.
+QUnit.test("Add factorial test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('5');
+    Calculator.factorial();
+
+    assert.equal(document.getElementById("screen").value, "120", "Passed - Expected 120");
+});
+
+
+//US5: As a user I want to be able to calculate the square root of a number by simply pressing a button.
+QUnit.test("Add square root test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('25');
+    Calculator.squareRoot();
+
+    assert.equal(document.getElementById("screen").value, "5", "Passed - Expected 5");
+});
+
+
+//US6: As a user I want to be able to calculate the square of a number by simply pressing a button.
+QUnit.test("Add square test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('5');
+    Calculator.square();
+
+    assert.equal(document.getElementById("screen").value, "25", "Passed - Expected 25");
+
+});
+//US7: As a user who sometimes makes mistakes when pressing buttons on the keypad, I want to be able to press a button that clears my current input, but not the stored procedure.
+QUnit.test("Add clear input test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('5');
+    Calculator.storeOperator('*');
+    Calculator.addDigit('6');
+    Calculator.clears();
+
+    assert.equal(document.getElementById("screen").value, "0", "Passed - Expected 0");
+});
+
+//US8: Bug Alert! There is a bug in the calculator app! As a careless user I want to be told that I just tried to divide by zero, which I should be told is not allowed.
+QUnit.test("Add divide by zero error", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('5');
+    Calculator.storeOperator('/');
+    Calculator.addDigit('0');
+    Calculator.calculate();
+
+    assert.equal(document.getElementById("screen").value, "DIVIDE BY ZERO ERROR", "Passed - Expected DIVIDE BY ZERO ERROR");
+});
+
+//US9: Bug Alert! As an easily confused user I don't want to be able to type numbers into the screen that causes some of the numbers to disappear off the screen, thus confusing me about what I actually typed.
+QUnit.test("Add character limit", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+  Calculator.addDigit('9');
+
+
+
+    assert.equal(document.getElementById("screen").value, "999999999999", "Passed - Expected 999999999999");
+});
